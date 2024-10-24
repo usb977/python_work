43a5d6061872a346dd37d0ea1efc279d37cc8e91
Dylan <1079282651@qq.com>
2024-10-24 20:51:52 +0800

    2024/10/24--002

---
+++ chapter_2/test_2_4.py
@@ -0,0 +1,4 @@
+name = 'Caterina James'
+print(name.lower())
+print(name.upper())
+print(name.title())
---
+++ chapter_2/test_2_7.py
@@ -0,0 +1,5 @@
+name = '\tAlbert Einstein\n'
+print(f"1:{name}")
+print(f"2:{name.lstrip()}")
+print(f"3:{name.rstrip()}")
+print(f"4:{name.strip()}")
