93f8e10e125703772840ffa7824a931ca8f40419
Dylan <1079282651@qq.com>
2025-09-04 07:56:15 +0800

    25/09/04-001 regular commit

--- chapter_6/6_01_alien.py
+++ chapter_6/6_01_alien.py
@@ -1,5 +1,12 @@
-alien_0 = {'color':'green',
-           'points':5}
+'''
+初步认识什么是字典
+列表用[]  字典用{}
+'''
 
-print(alien_0['color'])
-print(alien_0['points'])
+alien_0 = {'color':'green','points':5}   #“键值对”，用冒号分割   冒号后可以关联任意对象
+
+# print(alien_0['color'])     #通过键访问值的时候需要用方括号来索引
+# print(alien_0['points'])
+
+new_points = alien_0['points']
+print(f'你刚刚消灭了一个{alien_0["color"]}颜色的外星人，获得了{new_points}点分数！')
