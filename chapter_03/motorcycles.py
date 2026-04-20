1f3e4a8d5c18011c73dc40ee6df90ef311755cfd
Dylan <1079282651@qq.com>
2026-04-20 17:34:35 +0800

    temp:暂存代码--20260420--02

---
+++ chapter_03/motorcycles.py
@@ -0,0 +1,4 @@
+motorcycles = ['honda','yamaha','suzuki']
+print(motorcycles)
+motorcycles.insert(1,'ducati')
+print(motorcycles)
---
+++ chapter_03/pop_motorcycles.py
@@ -0,0 +1,6 @@
+motorcycles = ['honda','yamaha','suzuki']
+print(motorcycles)
+
+poped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(poped_motorcycle)
