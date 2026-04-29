c93575867d251d8041ad6e0d39859cb55637ae1f
Dylan <1079282651@qq.com>
2026-04-29 13:45:05 +0800

    feat:新增chapter_04的代码

--- chapter_04/foods.py
+++ chapter_04/foods.py
@@ -1,8 +1,9 @@
 my_foods = ['pizza','falafel','carrot','cake']
 friend_foods = my_foods[:]
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
 
 print('My favorite foods are:')
 print(my_foods)
-
 print("\nMy friend's favorite foods are:")
-print(friend_foods)
+print(friend_foods)     #两个是不同的列表
